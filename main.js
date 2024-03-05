4b91c645d512c6f518db66bd648a4bd8101f4196
Coding WIth Ahmed <155929233+ahmedprogrammer2005@users.noreply.github.com>
2024-03-06 02:02:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Assignment 1 (Alerts)
+
+// Answer 1 
+alert("Hello there! Welcome to our website.");
+
+// Answer 2
+alert("Error! Please Enter a valid password");
+
+// Answer 3
+alert("Welcome to JS Land...\nHappy Coding");
+
+// Answer 4
+alert("Welcome to JS Land");
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// Answer 5
+console.log("Hello... I can run JS through my web browse'r console");
